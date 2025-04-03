c3521b401ff615bfeaba083254bb79fc612b8c28
Oliver <supereinfach@gmx.net>
2025-04-03 12:04:41 +0200

    Changes are made

--- PFSS.lua
+++ PFSS.lua
@@ -54,56 +54,43 @@ end
 
 -- DECLARING VARIABLES
 local DistHome = {}
-local DistHome.current = 0
-local DistHome.max = 0
-local DistHome.total = 0
-
+	DistHome.current = 0
+	DistHome.max = 0
+	DistHome.total = 0 -- < not needed ?
 local Speed = {}
-local Speed.current = 0
-local Speed.currentID = getTelemetryId("GSpd")
-local Speed.max = 0
-local Speed.maxID = getTelemetryId("GSpd+")
-
+	Speed.current = 0
+	Speed.currentID = getTelemetryId("GSpd")
+	Speed.max = 0
+	Speed.maxID = getTelemetryId("GSpd+")
 local Altitude = {}
-local Altitude.current = 0
-local Altitude.currentID = gpsaltId = getTelemetryId("Alt")
-local Altitude.max = 0
-local Altitude.maxID = gpsaltId = getTelemetryId("Alt+")
-local Altitude.home = 0
--- IF "Alt" does not work, try GAlt or Tmp2
-
+	Altitude.current = 0
+	Altitude.currentID = getTelemetryId("Alt") -- MAYBE IF "Alt" does not work, try GAlt or Tmp2
+	Altitude.max = 0
+	Altitude.maxID = getTelemetryId("Alt+")
+	Altitude.home = 0
 local Battery = {}
-local Battery.current = 0
-local Battery.currentID = getTelemetryId("RxBt") > -1 and getTelemetryId("RxBt") or getTelemetryId("BtRx")
-local Battery.min = 99
-local Battery.minID = getTelemetryId("RxBt-") > -1 and getTelemetryId("RxBt-") or getTelemetryId("BtRx-")
-
+	Battery.current = 0
+	Battery.currentID = getTelemetryId("RxBt") > -1 and getTelemetryId("RxBt") or getTelemetryId("BtRx")
+	Battery.min = 99
+	Battery.minID = getTelemetryId("RxBt-") > -1 and getTelemetryId("RxBt-") or getTelemetryId("BtRx-")
 local Flightmode = {}
-local Flightmode.current = 0
-local Flightmode.currentID = getTelemetryId("FM")
-
+	Flightmode.current = 0
+	Flightmode.currentID = getTelemetryId("FM")
 local Sats = {}
-local Sats.current = 0
-local Sats.currentID = getTelemetryId("Sats")
-
+	Sats.current = 0
+	Sats.currentID = getTelemetryId("Sats")
 local GPS = {}
-local GPS.ID = getTelemetryId("GPS")
-
--- TO DO:  LINK QUALITY RECEIVER
-
+	GPS.Data = false
+	GPS.LatLon = 0
+	GPS.Lat = 0
+	GPS.Lon = 0
+	GPS.PrevLat = 0
+	GPS.PrevLon = 0
+	GPS.LatHome = 0
+	GPS.LonHome = 0
+	GPS.ID = getTelemetryId("GPS")
+local LinkQuality = 0
 local log_filename = "/LOGS/PFSS_Log.csv"
---local maxDistHome = 0 
---local maxAltitude = 0
---local maxAltitudeID = 0
---local maxSpeed = 0
---local maxSpeedID = 0
--- local StartAltitude = 0
---local BatMinimum = 99
---local BatNow = 0
---local BatID = 0
---local minBatID = 0
---local FlightMode = 0
---local FlightModeID = 0
 local DateTime = getDateTime()
 local Timer = 0
 local Time = 0
@@ -111,29 +98,12 @@ local LastTime = 0
 local Armed = false
 local Arming = false
 local ArmingTime = 0
-local gpsLAT = 0
-local gpsLON = 0
-local gpsLAT_H = 0
-local gpsLON_H = 0
-local gpsPrevLAT = 0
-local gpsPrevLON = 0
---local gpsSATS = 0
---local gpsALT = 0
---local gpsSpeed = 0
---local gpssatId = 0
---local gpsspeedId = 0
---local gpsaltId = 0
---local gpsDtH = 0
---local gpsTotalDist = 0
+local TotalDist = 0
 local update = true
 local reset = false
-local coordinates_prev = 0
-local coordinates_current = 0
-
 
 -- INIT
 local function init()  				
-
     -- WRITE HEADER, IF LOG FILE IS CREATED
 	if file_exists(log_filename)==false then
 	    file = io.open(log_filename, "a")
@@ -146,115 +116,134 @@ end
 
 -- BACKGROUND
 local function background()	
-	-- GET DATA
-    LastTime = Time
-	Time = getTime()
-	FlightMode = getValue(FlightModeID)
-	gpsLatLon = getValue(gpsId)
-	BatNow = getValue(BatID)
-	gpsSATS = getValue(gpssatId)
-	if string.len(gpsSATS) > 2 then		
-		gpsSATS = string.sub (gpsSATS, 3,6)		
+    -- CHECK CONNECTION
+	LinkQuality = getRSSI()
+	if LinkQuality > 0 then
+		Connected = true
 	else
-		gpsSATS = string.sub (gpsSATS, 0,3)		
-	end	    
-    
-    -- UPDATE ARMING STATUS
-    if (FlightMode == "AIR" or FlightMode == "STAB" or FlightMode == "ACRO" or FlightMode == "HOR") and not Armed and not Arming then
-        Arming = true
-        ArmingTime = getTime()
-    end
-    if Arming then
-        if getTime() - ArmingTime > 100 then
-            Armed = true
-            ArmingTime = 0
-            gpsDtH = 0
-		    gpsTotalDist = 0
-		    gpsLAT_H = 0
-		    gpsLON_H = 0
-	        maxDistHome = 0
-	        maxAltitude = 0
-	        maxSpeed = 0
-	        StartAltitude = 0
-	        Timer = 0
-	        BatMinimum = 99
-	        DateTime = getDateTime()
-	        local test = resetGlobalTimer()
-		    reset = true
-            Arming = false
-        end
-    end
-    if not (FlightMode == "AIR" or FlightMode == "STAB" or FlightMode == "ACRO" or FlightMode == "HOR") then
-        if Armed then
-        -- WRITE DATA TO LOG FILE
-            file = io.open(log_filename, "a")
-            io.write(file, string.sub(100+DateTime["day"],2).."."..string.sub(100+DateTime["mon"],2).."."..string.sub(DateTime["year"],3))
-            io.write(file, ";")
-            io.write(file, string.sub(100+DateTime["hour"],2)..":"..string.sub(100+DateTime["min"],2))
-            io.write(file, ";")
-            io.write(file, string.sub(100 + math.floor((Timer/100)/60),2)..":"..string.sub(100+math.floor(math.fmod((Timer/100),60)),2))
-            io.write(file, ";")
-            io.write(file, gpsTotalDist)
-            io.write(file, ";")
-            io.write(file, maxSpeed)
-            io.write(file, ";")
-            io.write(file, maxAltitude)
-            io.write(file, ";")
-            io.write(file, maxDistHome)
-            io.write(file, ";")
-            io.write(file, BatMinimum)
-            io.write(file, "\n")
-            io.close(file)
-        end
-        Armed = false
-    end
-
-    -- CHECK SATS AND UPDATE DISTANCE DATA
-   	if (tonumber(gpsSATS) >= 6) then
-	    if (type(gpsLatLon) == "table") then 			
-		    gpsLAT = rnd(gpsLatLon["lat"],6)
-		    gpsLON = rnd(gpsLatLon["lon"],6)		
-		    gpsSpeed = rnd(getValue(gpsspeedId), 1)
-		    gpsALT = rnd(getValue(gpsaltId),0)		
-		    maxSpeed = rnd(getValue(maxSpeedID),0)
-            maxAltitude = rnd(getValue(maxAltitudeID),0)
-            BatMinimum = rnd(getValue(minBatID),2)
-		    if Armed then
-     		   Timer = Timer + (Time - LastTime)
-		    end
-		    
-		    -- SET START POSITION
-		    if (reset == true) then
-			    gpsLAT_H = rnd(gpsLatLon["lat"],6)
-			    gpsLON_H = rnd(gpsLatLon["lon"],6)	
-			    reset = false
-		    end		
-
-		    update = true	
-	    else
-	        -- NO TELEMETRY (GPS) DATA
-		    update = false
-	    end
+		Connected = false
+	end
+	LastTime = Time
+	Time = getTime()
 	
-	-- CALCULATE DISTANCE HOME / TRIP
-		if (gpsLAT ~= gpsPrevLAT) and (gpsLON ~=  gpsPrevLON) then
-			if (gpsLAT_H ~= 0) and  (gpsLON_H ~= 0) then 
+	-- IF CONNECTED, GATHER INFORMATION
+	if Connected then
+		Flightmode.current = getValue(Flightmode.currentID)
+		GPS.LatLon = getValue(GPS.ID)
+		Battery.current = getValue(Battery.currentID)
+		Battery.min = getValue(Battery.minID)
+		Sats.current = getValue(Sats.currentID)
+		--Speed.max = rnd(getValue(Speed.maxID),0)					
+		--Altitude.max = rnd(getValue(Altitude.maxID),0)
+		if string.len(Sats.current) > 2 then		
+			Sats.current = string.sub (Sats.current, 3,6)		
+		else
+			Sats.current = string.sub (Sats.current, 0,3)		
+		end	    
+	
+		-- IF GPS DATA AVAILABLE, GATHER GPS DATA
+		if (tonumber(Sats.current) >= 6) then
+			if (type(GPS.LatLon) == "table") then 			
+				GPS.Lat = rnd(GPS.LatLon["lat"],6)
+				GPS.Lon = rnd(GPS.LatLon["lon"],6)		
+				Speed.current = rnd(getValue(Speed.currentID), 1)
+				Altitude.current = rnd(getValue(Altitude.currentID),0)
 
+				-- SET START POSITION
+				if (reset == true) then
+					GPS.LatHome = GPS.Lat
+					GPS.LonHome = GPS.Lon
+					reset = false
+				end		
+
+				GPS.Data = true	
+			else
+				-- NO TELEMETRY (GPS) DATA
+				GPS.Data = false
+			end
+		end
+
+		-- UPDATE ARMING STATUS
+		if (Flightmode.current == "AIR" or Flightmode.current == "STAB" or Flightmode.current == "ACRO" or Flightmode.current == "HOR") and not Armed and not Arming then
+			Arming = true
+			-- local resetTimer = resetGlobalTimer()
+			ArmingTime = getTime()
+			--local resetAltitude = resetSensor(Altitude.maxID)
+		end
+
+		if Arming then
+			if getTime() - ArmingTime > 100 then
+				-- local resetTimer = resetGlobalTimer()
+				-- local resetAltitude = resetSensor(Altitude.maxID)
+				Armed = true
+				ArmingTime = 0
+				Timer = 0		
+				TotalDist = 0
+				DistHome.current = 0
+				DistHome.max = 0
+				Altitude.max = 0
+				Speed.max = 0
+				GPS.LatHome = 0
+				GPS.LonHome = 0
+				Battery.min = 99
+				DateTime = getDateTime()
+				reset = true
+				Arming = false
+			end
+		end
+		if not (Flightmode.current == "AIR" or Flightmode.current == "STAB" or Flightmode.current == "ACRO" or Flightmode.current == "HOR") then
+			if Armed then
+			-- WRITE DATA TO LOG FILE
+				file = io.open(log_filename, "a")
+				io.write(file, string.sub(100+DateTime["day"],2).."."..string.sub(100+DateTime["mon"],2).."."..string.sub(DateTime["year"],3))
+				io.write(file, ";")
+				io.write(file, string.sub(100+DateTime["hour"],2)..":"..string.sub(100+DateTime["min"],2))
+				io.write(file, ";")
+				io.write(file, string.sub(100 + math.floor((Timer/100)/60),2)..":"..string.sub(100+math.floor(math.fmod((Timer/100),60)),2))
+				io.write(file, ";")
+				io.write(file, TotalDist)
+				io.write(file, ";")
+				io.write(file, Speed.max)
+				io.write(file, ";")
+				io.write(file, Altitude.max)
+				io.write(file, ";")
+				io.write(file, DistHome.max)
+				io.write(file, ";")
+				io.write(file, Battery.min)
+				io.write(file, "\n")
+				io.close(file)
+			end
+			Armed = false
+		end
+
+		-- UPDATE TIMER, IF ARMED
+		if Armed then
+			Timer = Timer + (Time - LastTime)
+		end
+		
+		-- CALCULATE DISTANCE HOME / TRIP
+		if (GPS.Lat ~= GPS.PrevLat) and (GPS.Lon ~= GPS.PrevLon) then
+			if (GPS.LatHome ~= 0) and  (GPS.LonHome ~= 0) then 
+				-- SPEED / ALTITUDE
+				if Armed then
+					if Speed.current > Speed.max then Speed.max = Speed.current end
+					if Altitude.current > Altitude.max then Altitude.max = Altitude.current end
+				end
 				-- DISTANCE HOME
-				gpsDtH = rnd(calc_Distance(gpsLAT, gpsLON, gpsLAT_H, gpsLON_H),3)
-				if (gpsDtH > maxDistHome) and gpsDtH ~= nil then
-				    maxDistHome = gpsDtH
+				DistHome.current = rnd(calc_Distance(GPS.Lat, GPS.Lon, GPS.LatHome, GPS.LonHome),3)
+				if (DistHome.current > DistHome.max) and DistHome.current ~= nil then
+					DistHome.max = DistHome.current
 				end				
-				gpsDtH = string.format("%.3f",gpsDtH)
+				DistHome.current = string.format("%.3f",DistHome.current)
 				
 				-- DISTANCE TRIP
-				if (gpsPrevLAT ~= 0) and  (gpsPrevLON ~= 0) and (gpsLAT ~= 0) and  (gpsLON ~= 0)then	
-					gpsTotalDist =  rnd(tonumber(gpsTotalDist) + calc_Distance(gpsLAT,gpsLON,gpsPrevLAT,gpsPrevLON),5) -- changed from 2 to 5
-					gpsTotalDist = string.format("%.3f",gpsTotalDist)					
+				if (GPS.PrevLat ~= 0) and  (GPS.PrevLon ~= 0) and (GPS.Lat ~= 0) and  (GPS.Lon ~= 0)then	
+					TotalDist =  rnd(tonumber(TotalDist) + calc_Distance(GPS.Lat,GPS.Lon,GPS.PrevLat,GPS.PrevLon),5) 
+					TotalDist = string.format("%.3f",TotalDist)					
 				end
 			end
-			gpsPrevLAT = gpsLAT
-			gpsPrevLON = gpsLON	
+			GPS.PrevLat = GPS.Lat
+			GPS.PrevLon = GPS.Lon
 		end
 	end
 end
@@ -268,41 +257,41 @@ local function run(event)
 	-- DRAW SCREEN
 	lcd.drawText(2,1,"Flight Mode: " ,SMLSIZE)
 	lcd.drawFilledRectangle(1,0, 126, 8, GREY_DEFAULT)
-	lcd.drawText(60,1,FlightMode ,SMLSIZE + INVERS)
+	lcd.drawText(60,1,Flightmode.current ,SMLSIZE + INVERS)
 	lcd.drawText(90,1,tostring(Armed) ,SMLSIZE + INVERS)
 	lcd.drawText(2,9, "Sats:", SMLSIZE)	
-	lcd.drawText(30,9, gpsSATS, SMLSIZE)
+	lcd.drawText(30,9, Sats.current, SMLSIZE)
 	lcd.drawText(60,9, "Armed:", SMLSIZE)
 	lcd.drawText(90,9, string.sub(100 + math.floor((Timer/100)/60),2)..":"..string.sub(100+math.floor(math.fmod((Timer/100),60)),2), SMLSIZE)
 	lcd.drawText(2,23, "Trip:", SMLSIZE)
-	lcd.drawText(30,23, gpsTotalDist, SMLSIZE)
+	lcd.drawText(30,23, TotalDist, SMLSIZE)
 	lcd.drawText(2,16, "Home:", SMLSIZE)
-	lcd.drawText(30,16, string.format("%.5f", gpsLAT_H) .. " / " .. string.format("%.5f", gpsLON_H), SMLSIZE)
+	lcd.drawText(30,16, string.format("%.5f", GPS.LatHome) .. " / " .. string.format("%.5f", GPS.LonHome), SMLSIZE)
 	lcd.drawText(2,30, "Speed:", SMLSIZE)
-	lcd.drawText(30,30, gpsSpeed,SMLSIZE)
+	lcd.drawText(30,30, Speed.current,SMLSIZE)
 	lcd.drawText(62,30, "max:", SMLSIZE)
-	lcd.drawText(90,30, maxSpeed,SMLSIZE)
+	lcd.drawText(90,30, Speed.max,SMLSIZE)
 	lcd.drawText(2,37, "Alt:", SMLSIZE)
-	lcd.drawText(30,37, gpsALT,SMLSIZE)
+	lcd.drawText(30,37, Altitude.current,SMLSIZE)
 	lcd.drawText(62,37, "max:", SMLSIZE)
-	lcd.drawText(90,37, maxAltitude,SMLSIZE)
+	lcd.drawText(90,37, Altitude.max,SMLSIZE)
 	lcd.drawText(2,44, "DtH:", SMLSIZE)
-	lcd.drawText(30,44, gpsDtH, SMLSIZE)
+	lcd.drawText(30,44, DistHome.current, SMLSIZE)
 	lcd.drawText(62,44, "max:", SMLSIZE)
-	lcd.drawText(90,44, maxDistHome, SMLSIZE)
+	lcd.drawText(90,44, DistHome.max, SMLSIZE)
 	lcd.drawText(2,51, "Bat:", SMLSIZE)
-	lcd.drawText(30,51, string.format("%.2f",BatNow), SMLSIZE)
+	lcd.drawText(30,51, string.format("%.2f",Battery.current), SMLSIZE)
 	lcd.drawText(62,51, "min:", SMLSIZE)
-	lcd.drawText(90,51, string.format("%.2f",BatMinimum), SMLSIZE)
+	lcd.drawText(90,51, string.format("%.2f",Battery.min), SMLSIZE)
 	lcd.drawText(27,58, string.sub(100+DateTime["day"],2).."."..string.sub(100+DateTime["mon"],2).."."..string.sub(DateTime["year"],3).." - "..string.sub(100+DateTime["hour"],2)..":"..string.sub(100+DateTime["min"],2), SMLSIZE)
 
     -- BLINK IF LOW SATS
-	if (tonumber(gpsSATS) < 6) then
+	if (tonumber(Sats.current) < 6) then
 		lcd.drawText(60,23, "< 6 SATS!", SMLSIZE + INVERS + BLINK)
 	end
 		
 	-- BLINK IF NO TELEMETRY
-	if update == false then
+	if Connected == false then
 		lcd.drawText(60,23, "NO TELEMETRY!", SMLSIZE + INVERS + BLINK)		
 	end	
 end
